332ef4c7c5438ecef645e8d0a44878c315bdbab2
SoHee Jeong <jsohee98@naver.com>
2021-12-20 03:02:22 +0900

    사용자 아바타 보여주기

--- shop/models.py
+++ shop/models.py
@@ -50,3 +50,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.menu.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/384/558d57b5d8a79409/svg/{self.author.email}'
