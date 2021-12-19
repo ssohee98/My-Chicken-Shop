ebdc7e7e62b50b81796c285ce8597d8a0d5aed4e
SoHee Jeong <jsohee98@naver.com>
2021-12-19 23:36:37 +0900

    포스트 작성 페이지(로그인한 사용자만)

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('create_menu/', views.MenuCreate.as_view()),
     path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.MenuDetail.as_view()),
     path('', views.MenuList.as_view()),
