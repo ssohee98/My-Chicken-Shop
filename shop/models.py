caddf1d6b3e46c1cd65bb9a226c32401dbd0ab82
SoHee Jeong <jsohee98@naver.com>
2021-12-19 22:55:05 +0900

    작성자 정보 출력

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Menu(models.Model):
     title = models.CharField(max_length=30)
@@ -9,10 +10,10 @@ class Menu(models.Model):
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # author: 추후 작성 예정
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/shop/{self.pk}/'
