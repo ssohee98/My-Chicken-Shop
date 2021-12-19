2531d97688c47a465ecd839d69efa70ba6e26b8e
SoHee Jeong <jsohee98@naver.com>
2021-12-19 23:48:01 +0900

    메뉴 작성 페이지 권한 설정

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Menu, Category
 
 class MenuList(ListView):
@@ -21,13 +21,16 @@ class MenuDetail(DetailView):
         context['no_category_menu_count'] = Menu.objects.filter(category=None).count()
         return context
 
-class MenuCreate(LoginRequiredMixin, CreateView):
+class MenuCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Menu
     fields = ['title', 'hook_text', 'content', 'head_image', 'price', 'category']
 
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
     def form_valid(self, form):
         current_user = self.request.user
-        if current_user.is_authenticated:
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             form.instance.author = current_user
             return super(MenuCreate, self).form_valid(form)
         else:
